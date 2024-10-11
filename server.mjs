328330e7844c4633bc6c5a99247e47c7cdeaf33e
HasnainAli313 <aliorakzai407@gmail.com>
2024-10-11 22:17:21 +0500

    added mongodb api

--- server.mjs
+++ server.mjs
@@ -5,3 +5,5 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const mongoUri = 'mongodb://localhost:27017/user_management'; // or use your MongoDB Atlas URI
