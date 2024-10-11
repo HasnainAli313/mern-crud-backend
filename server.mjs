59bc938f35a06676a41537295943c624df757a20
HasnainAli313 <aliorakzai407@gmail.com>
2024-10-11 22:23:56 +0500

    done with post method

--- server.mjs
+++ server.mjs
@@ -23,3 +23,13 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model('User', userSchema);
+
+app.post('/users', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
