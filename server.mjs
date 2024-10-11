51dcb1d472a2794bc037ed02aacd92762d4a2a40
HasnainAli313 <aliorakzai407@gmail.com>
2024-10-11 23:51:50 +0500

    done

--- server.mjs
+++ server.mjs
@@ -1,3 +1,106 @@
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// // const mongoUri = 'mongodb://localhost:27017/user_management'; // or use your MongoDB Atlas URI
+// const mongoUri = 'mongodb+srv://aliorakzai407:<webdeveloper>@cluster0.uwz8y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // or use your MongoDB Atlas URI
+
+// mongoose.connect(mongoUri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }).then(() => {
+//     console.log('Connected to MongoDB');
+// }).catch(err => {
+//     console.error('Error connecting to MongoDB:', err.message);
+// });
+
+// const userSchema = new mongoose.Schema({
+//     name: String,
+//     email: String,
+// });
+
+// const User = mongoose.model('User', userSchema);
+
+// app.post('/users', async (req, res) => {
+//     try {
+//         const user = new User(req.body);
+//         await user.save();
+//         res.status(201).send(user);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// });
+
+// app.get('/users', async (req, res) => {
+//     try {
+//         const users = await User.find({});
+//         res.status(200).send(users);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// app.get('/users/:id', async (req, res) => {
+//     try {
+//         const user = await User.findById(req.params.id);
+//         if (!user) {
+//             return res.status(404).send();
+//         }
+//         res.status(200).send(user);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// app.patch('/users/:id', async (req, res) => {
+//     const updates = Object.keys(req.body);
+//     const allowedUpdates = ['name', 'email'];
+//     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+//     if (!isValidOperation) {
+//         return res.status(400).send({ error: 'Invalid updates!' });
+//     }
+
+//     try {
+//         const user = await User.findById(req.params.id);
+
+//         if (!user) {
+//             return res.status(404).send();
+//         }
+
+//         updates.forEach((update) => user[update] = req.body[update]);
+//         await user.save();
+//         res.status(200).send(user);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// });
+
+// app.delete('/users/:id', async (req, res) => {
+//     try {
+//         const user = await User.findByIdAndDelete(req.params.id);
+
+//         if (!user) {
+//             return res.status(404).send();
+//         }
+
+//         res.status(200).send(user);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -6,12 +109,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoUri = 'mongodb://localhost:27017/user_management'; // or use your MongoDB Atlas URI
+// Update the connection string with correct credentials
 
-mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
+const mongoUri = 'mongodb+srv://hasnainali:webdeveloper@cluster0.uwz8y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+
+mongoose.connect(mongoUri).then(() => {
     console.log('Connected to MongoDB');
 }).catch(err => {
     console.error('Error connecting to MongoDB:', err.message);
@@ -91,4 +194,9 @@ app.delete('/users/:id', async (req, res) => {
     } catch (error) {
         res.status(500).send(error);
     }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
